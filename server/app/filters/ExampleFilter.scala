155864e51b9916b6115e5fb05c347568b3c426e5
jianghang <hangscer@gmail.com>
2018-03-18 17:13:02 +0800

    log坑

--- server/app/filters/ExampleFilter.scala
+++ server/app/filters/ExampleFilter.scala
@@ -1,12 +1,11 @@
 package filters
 
 import java.util.Date
-import javax.inject._
 
+import javax.inject._
 import play.api.mvc._
 
 import scala.concurrent.ExecutionContext
-import scala.util.{Failure, Success}
 
 /**
   * This is a simple filter that adds a header to all requests. It's
@@ -19,16 +18,9 @@ import scala.util.{Failure, Success}
 @Singleton
 class ExampleFilter @Inject()(implicit ec: ExecutionContext) extends EssentialFilter {
 
-  import util.actor.ActorMain._
-
   override def apply(next: EssentialAction) = EssentialAction { request =>
     next(request).map { result =>
-      result.body.dataStream.runFold("")((a, z) => a + z.utf8String).onComplete {
-        case Success(body) =>
-
-          println(s"timestamp:${new Date(System.currentTimeMillis())} , method:${Console.RED}${request.method.toUpperCase}${Console.RESET} , path:${Console.GREEN}${request.path}${Console.RESET} , session:${Console.BLUE}${request.session}${Console.RESET} , flash:${Console.MAGENTA}${request.flash}${Console.RESET}")
-        case Failure(ex) =>
-      }
+      println(s"timestamp:${new Date(System.currentTimeMillis())} , method:${Console.RED}${request.method.toUpperCase}${Console.RESET} , path:${Console.GREEN}${request.path}${Console.RESET} , session:${Console.BLUE}${request.session}${Console.RESET} , flash:${Console.MAGENTA}${request.flash}${Console.RESET}")
       result.withHeaders("X-ExampleFilter" -> "foo")
     }
   }
